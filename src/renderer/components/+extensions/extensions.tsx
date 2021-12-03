7363b80223d5fc21064db3fb17deb9eda18f233a
Sebastian Malton <sebastian@malton.name>
2021-12-03 10:38:04 -0500

    Add support for customizing the extension install registry URL
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+extensions/extensions.tsx
+++ src/renderer/components/+extensions/extensions.tsx
@@ -31,7 +31,7 @@ import path from "path";
 import React from "react";
 import { SemVer } from "semver";
 import URLParse from "url-parse";
-import { Disposer, disposer, downloadFile, downloadJson, ExtendableDisposer, extractTar, listTarEntries, noop, readFileFromTar } from "../../../common/utils";
+import { Disposer, disposer, downloadFile, downloadJson, ExtendableDisposer, extractTar, listTarEntries, noop, promiseExecFile, readFileFromTar } from "../../../common/utils";
 import { ExtensionDiscovery, InstalledExtension, manifestFilename } from "../../../extensions/extension-discovery";
 import { ExtensionLoader } from "../../../extensions/extension-loader";
 import { extensionDisplayName, LensExtensionId, LensExtensionManifest, sanitizeExtensionName } from "../../../extensions/lens-extension";
@@ -48,6 +48,8 @@ import { SettingLayout } from "../layout/setting-layout";
 import { docsUrl } from "../../../common/vars";
 import { dialog } from "../../remote-helpers";
 import { AppPaths } from "../../../common/app-paths";
+import { UserStore } from "../../../common/user-store";
+import { defaultExtensionRegistryUrl, ExtensionRegistryLocation } from "../../../common/user-store/preferences-helpers";
 
 function getMessageFromError(error: any): string {
   if (!error || typeof error !== "object") {
@@ -300,16 +302,58 @@ async function unpackExtension(request: InstallRequestValidated, disposeDownload
   }
 }
 
+async function getBaseRegistryUrl(): Promise<string> {
+  const { extensionRegistryUrl } = UserStore.getInstance();
+
+  switch (extensionRegistryUrl.location) {
+    case ExtensionRegistryLocation.CUSTOM:
+      return extensionRegistryUrl.customUrl;
+
+    case ExtensionRegistryLocation.NPMRC: {
+      try {
+        const filteredEnv = Object.fromEntries(
+          Object.entries(process.env)
+            .filter(([key]) => !key.startsWith("npm")),
+        );
+        const { stdout } = await promiseExecFile("npm", ["config", "get", "registry"], { env: filteredEnv });
+
+        return stdout.trim();
+      } catch (error) {
+        console.warn("[EXTENSIONS]: failed to get configured registry from .npmrc", error);
+        // fallthrough
+      }
+    }
+    default:
+    case ExtensionRegistryLocation.DEFAULT:
+      return defaultExtensionRegistryUrl;
+  }
+}
+
 export async function attemptInstallByInfo({ name, version, requireConfirmation = false }: ExtensionInfo) {
   const disposer = ExtensionInstallationStateStore.startPreInstall();
-  const registryUrl = new URLParse("https://registry.npmjs.com").set("pathname", name).toString();
-  const { promise } = downloadJson({ url: registryUrl });
-  const json = await promise.catch(console.error);
+  const baseUrl = await getBaseRegistryUrl();
+  const registryUrl = new URLParse(baseUrl).set("pathname", name).toString();
+  let json: any;
 
-  if (!json || json.error || typeof json.versions !== "object" || !json.versions) {
-    const message = json?.error ? `: ${json.error}` : "";
+  try {
+    json = await downloadJson({ url: registryUrl }).promise;
 
-    Notifications.error(`Failed to get registry information for that extension${message}`);
+    if (!json || json.error || typeof json.versions !== "object" || !json.versions) {
+      const message = json?.error ? `: ${json.error}` : "";
+
+      Notifications.error(`Failed to get registry information for that extension${message}`);
+
+      return disposer();
+    }
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      // assume invalid JSON
+      console.warn("Set registry has invalid json", { url: baseUrl }, error);
+      Notifications.error("Failed to get valid registry information for that extension. Registry did not return valid JSON");
+    } else {
+      console.error("Failed to download registry information", error);
+      Notifications.error(`Failed to get valid registry information for that extension. ${error}`);
+    }
 
     return disposer();
   }
